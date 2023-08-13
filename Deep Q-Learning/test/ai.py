f81ed7fbaf398b68ac1677020f2e716c63301499
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-08-13 12:44:20 +0600

    ai

---
+++ Deep Q-Learning/test/ai.py
@@ -0,0 +1,21 @@
+# LIBRARIES
+import numpy as np
+import random
+import os
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+# Making architecture of Neural Network
+
+class Network(nn.Module):
+
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(input_size, 15)
+
