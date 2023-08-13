8e65677c2e1aa179daf1021ea083ba3f9a0fae35
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-08-13 13:06:15 +0600

    experience replay

--- Deep Q-Learning/test/ai.py
+++ Deep Q-Learning/test/ai.py
@@ -17,5 +17,20 @@ class Network(nn.Module):
         self.input_size = input_size
         self.nb_action = nb_action
         self.fc1 = nn.Linear(input_size, 30)
-        self.fc2 = nn.Linear(input_size, 15)
+        self.fc2 = nn.Linear(30, nb_action)
 
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+    
+# Experience Replay
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, event):
+        self.memory.append()
